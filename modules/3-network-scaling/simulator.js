56318857e4504d64d0bf6f8af50ed824f931925a
Douglas G. Moore <doug@dglmoore.com>
2018-12-17 23:41:25 -0700

    Convince jsnetworkx to draw graphs
    It turns out that jsnetworkx requires D3 v3, which is two versions
    behind. We also fixed a few syntactic issues.

--- modules/3-network-scaling/simulator.js
+++ modules/3-network-scaling/simulator.js
@@ -1,55 +1,59 @@
-<script src="jsnetworkx.js"></script>
-<script src="https://d3js.org/d3.v5.min.js"></script>
+const Simulator = function(G, id = '#simulator') {
+    const simId = id;
+    const div = d3.select(simId);
 
+    const draw_graph = function() {
+        const width = +div.attr('width');
+        const height = +div.attr('height');
 
-var G= new jsnx.Graph();
-var G = jsnx.binomialGraph(6, 0.3);
+        jsnx.draw(G, {
+            element: simId,
 
-const app = function (G){
-	let svg = d3.select('#simulator');
-	const draw_graph = function() {
-		let width = svg.attr('width');
-		let height = svg.attr('height');
-	
-		jsnx.draw(G, {
-			element: '#simulator',
-			weighted= false,
-			edgeStyle: {
-				'stroke-width': 10
-			}
-	});
+            width: width,
+            height: height,
+
+            weighted: false,
+            edgeStyle: {
+                'stroke-width': 10
+            }
+        });
+    };
+
+    return { draw_graph };
+};
 
 (function() {
-	app.draw_graph(G);
+    var G = jsnx.binomialGraph(6, 0.3);
 
-	var cluster=jsnx.averageClustering(G);
-	console.log(cluster);
-	var degrees=G.degree();
-	var degreesum=0;
-	for (let degree of degrees.values()) {   
-		degreesum += degree;
-	}
-	var nodesnumber=degrees.size;
-	var meandegree=degreesum/nodesnumber;
-	console.log(meandegree);
+    let app = Simulator(G);
 
+    app.draw_graph();
 
-	var shortestpathsum=0;
-	var nodes=G.nodes();
-	var pathnumber=0;
-	for (let node1 of nodes.values()){
-		for (let node2 of nodes.values()){
-			var shortestpath=jsnx.shortestPathLength(G,{source:node1,target:node2});
-			shortestpathsum += shortestpath;
-			pathnumber += 1;
-		}
+    var cluster = jsnx.averageClustering(G);
+    console.log(cluster);
 
-	}
+    var degrees = G.degree();
+    var degreesum = 0;
+    for (let degree of degrees.values()) {
+        degreesum += degree;
+    }
+    var meandegree = degreesum / degrees.size;
+    console.log(meandegree);
 
+    var shortestpathsum = 0;
+    var nodes = G.nodes();
+    var pathnumber = 0;
+    for (let node1 of nodes.values()){
+        for (let node2 of nodes.values()){
+            var shortestpath = jsnx.shortestPathLength(G,{source:node1,target:node2});
+            shortestpathsum += shortestpath;
+            pathnumber += 1;
+        }
+    }
 
-	var averagelength=shortestpathsum/pathnumber;
-	console.log(averagelength);
+    var averagelength = shortestpathsum / pathnumber;
+    console.log(averagelength);
 
-	var distribution=jsnx.degreeHistogram(G);
-	console.log(distribution);
+    var distribution = jsnx.degreeHistogram(G);
+    console.log(distribution);
 }());
--- modules/3-network-scaling/simulatortest.js
+++ modules/3-network-scaling/simulatortest.js
@@ -1,46 +1,37 @@
-<script src="jsnetworkx.js"></script>
-<script src="https://d3js.org/d3.v5.min.js"></script>
-
-
-var G= new jsnx.Graph();
 var G = jsnx.binomialGraph(6, 0.3);
 
 jsnx.draw(G, {
-	element: '#simulator',
-	weighted= false,
-	edgeStyle: {
-			'stroke-width': 10
-	}
+    element: '#simulator',
+    weighted: false,
+    edgeStyle: {
+        'stroke-width': 10
+    }
 });
 
-var cluster=jsnx.averageClustering(G);
+var cluster = jsnx.averageClustering(G);
 console.log(cluster);
-var degrees=G.degree();
-var degreesum=0;
-for (let degree of degrees.values()) {   
-	degreesum += degree;
+
+var degrees = G.degree();
+var degreesum = 0;
+for (let degree of degrees.values()) {
+    degreesum += degree;
 }
-var nodesnumber=degrees.size;
-var meandegree=degreesum/nodesnumber;
+var meandegree = degreesum / degrees.size;
 console.log(meandegree);
 
-
-var shortestpathsum=0;
-var nodes=G.nodes();
-var pathnumber=0;
+var shortestpathsum = 0;
+var nodes = G.nodes();
+var pathnumber = 0;
 for (let node1 of nodes.values()){
-	for (let node2 of nodes.values()){
-		var shortestpath=jsnx.shortestPathLength(G,{source:node1,target:node2});
-		shortestpathsum += shortestpath;
-		pathnumber += 1;
-	}
-
+    for (let node2 of nodes.values()){
+        var shortestpath = jsnx.shortestPathLength(G,{source:node1,target:node2});
+        shortestpathsum += shortestpath;
+        pathnumber += 1;
+    }
 }
 
+var averagelength = shortestpathsum / pathnumber;
+console.log(averagelength);
 
-	var averagelength=shortestpathsum/pathnumber;
-	console.log(averagelength);
-
-	var distribution=jsnx.degreeHistogram(G);
-	console.log(distribution);
-}());
+var distribution = jsnx.degreeHistogram(G);
+console.log(distribution);
