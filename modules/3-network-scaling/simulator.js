ad694dfddfcc9e7fe3d816a487b461941f6cc237
Tessa <tfisher4@asu.edu>
2018-12-17 17:13:00 -0700

    Added test files to try to get JS and HTML to talk to each other

--- modules/3-network-scaling/simulator.js
+++ modules/3-network-scaling/simulator.js
@@ -2,44 +2,54 @@
 <script src="https://d3js.org/d3.v5.min.js"></script>
 
 
-
 var G= new jsnx.Graph();
+var G = jsnx.binomialGraph(6, 0.3);
 
-var G = jsnx.binomialGraph(6,0.8); //for back-end testing purposes
-//input nodes and edges from front end
+const app = function (G){
+	let svg = d3.select('#simulator');
+	const draw_graph = function() {
+		let width = svg.attr('width');
+		let height = svg.attr('height');
+	
+		jsnx.draw(G, {
+			element: '#simulator',
+			weighted= false,
+			edgeStyle: {
+				'stroke-width': 10
+			}
+	});
 
-//G.addNode();
-//G.addEdge();
+(function() {
+	app.draw_graph(G);
+
+	var cluster=jsnx.averageClustering(G);
+	console.log(cluster);
+	var degrees=G.degree();
+	var degreesum=0;
+	for (let degree of degrees.values()) {   
+		degreesum += degree;
+	}
+	var nodesnumber=degrees.size;
+	var meandegree=degreesum/nodesnumber;
+	console.log(meandegree);
 
 
+	var shortestpathsum=0;
+	var nodes=G.nodes();
+	var pathnumber=0;
+	for (let node1 of nodes.values()){
+		for (let node2 of nodes.values()){
+			var shortestpath=jsnx.shortestPathLength(G,{source:node1,target:node2});
+			shortestpathsum += shortestpath;
+			pathnumber += 1;
+		}
 
-var cluster=jsnx.averageClustering(G);
-console.log(cluster);
-var degrees=G.degree();
-var degreesum=0;
-for (let degree of degrees.values()) {   
-	degreesum += degree;
-}
-var nodesnumber=degrees.size;
-var meandegree=degreesum/nodesnumber;
-console.log(meandegree);
-
-
-var shortestpathsum=0;
-var nodes=G.nodes();
-var pathnumber=0;
-for (let node1 of nodes.values()){
-	for (let node2 of nodes.values()){
-		var shortestpath=jsnx.shortestPathLength(G,{source:node1,target:node2});
-		shortestpathsum += shortestpath;
-		pathnumber += 1;
 	}
 
-}
 
+	var averagelength=shortestpathsum/pathnumber;
+	console.log(averagelength);
 
-var averagelength=shortestpathsum/pathnumber;
-console.log(averagelength);
-
-var distribution=jsnx.degreeHistogram(G);
-console.log(distribution);
+	var distribution=jsnx.degreeHistogram(G);
+	console.log(distribution);
+}());
